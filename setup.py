983244612270274fa96efae3626a828779e854c8
Ryan Shihabi <ryanshihabi10@gmail.com>
2021-09-14 21:36:48 -0700

    add playlist retrieval

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
 import os
+from os import path
 import json
 import datetime
 
+playlist = "PLisfUdjySbZVoTRbAlfObs8dI-cb-gWk-"
+
 timestamp = str(datetime.datetime.now())[:10]
 
-os.system(f"youtube-dl --dump-single-json PLisfUdjySbZVoTRbAlfObs8dI-cb-gWk- > {timestamp}.json")
+if path.exists(f"data/{timestamp}.json") == False:
+    print("Grabbing playlist information...")
+    os.system(f"youtube-dl --dump-single-json {playlist} > data/{timestamp}.json")
 
-#iterate through videos depending on what the playlist requirements provide
+f = open(f"data/{timestamp}.json",)
 
-# for i in range(num_videos+1):
-#     os.system("youtube-dl --flat-playlist PLisfUdjySbZVoTRbAlfObs8dI-cb-gWk- > playlist.txt")
+data = json.load(f)
+
+f.close()
+
+print(len(data["entries"]))
+
+videos = []
+
+for i in data["entries"]:
+    videos.append([i['title'], i['id']])
+
+print(videos)
+
+# once video is downloaded, add to downloaded.txt
