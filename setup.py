dc0cb86b8d5f6c916f7e280b551d307957f8d0ca
Ryan Shihabi <ryanshihabi10@gmail.com>
2021-09-15 08:55:30 -0700

    playlist cleaning

--- setup.py
+++ setup.py
@@ -1,17 +1,37 @@
 import os
 from os import path
+import pathlib
 import json
 import datetime
+
+file_count = 0
 
 playlist = "PLisfUdjySbZVoTRbAlfObs8dI-cb-gWk-"
 
 timestamp = str(datetime.datetime.now())[:10]
 
-if path.exists(f"data/{timestamp}.json") == False:
+if path.exists(f"data/playlist/{timestamp}.json") == False:
     print("Grabbing playlist information...")
-    os.system(f"youtube-dl --dump-single-json {playlist} > data/{timestamp}.json")
+    os.system(f"youtube-dl --dump-single-json {playlist} > data/playlist/{timestamp}.json")
 
-f = open(f"data/{timestamp}.json",)
+dates = []
+earliest_date = ""
+for path in pathlib.Path("./data/playlist").iterdir():
+    date = str(path)[14:-5]
+    dates.append(date)
+
+dates = sorted(dates, key=lambda x: datetime.datetime.strptime(x, "%Y-%m-%d"))
+
+print(dates)
+
+while len(dates) > 2:
+    print("Cleaning list...")
+    os.remove(f"data/playlist/{dates[0]}.json")
+    dates.pop(0)
+
+# print("Num of files: ", file_count){value for value in variable}
+
+f = open(f"data/playlist/{timestamp}.json",)
 
 data = json.load(f)
 
@@ -24,6 +44,6 @@ videos = []
 for i in data["entries"]:
     videos.append([i['title'], i['id']])
 
-print(videos)
+# print(videos)
 
 # once video is downloaded, add to downloaded.txt
