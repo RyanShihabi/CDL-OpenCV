15b6b7d89cfb06f94a1a93841f3a7045611c7458
Ryan Shihabi <ryanshihabi10@gmail.com>
2021-12-16 21:21:02 -0800

    feature extraction

--- CV/location.py
+++ CV/location.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-# import pytesseract
 
 cap = cv2.VideoCapture('./videos/demo.mp4')
 
@@ -12,18 +11,28 @@ while cap.isOpened():
     if ret:
         map = frame[675:1060, 25:700]
 
+        focus = frame[725:1040, 35:445]
+
+        # cv2.imshow("focused", focus)
+
         mblur = cv2.medianBlur(map, 3)
+        fblur = cv2.medianBlur(focus, 3)
 
         gray = cv2.cvtColor(mblur, cv2.COLOR_BGR2GRAY)
-        # hsl = cv2.cvtColor(map, cv2.COLOR_BGR2HLS)
-        hsv = cv2.cvtColor(mblur, cv2.COLOR_BGR2HSV)
 
-        white_lower = np.array([0, 0, 160])
-        white_upper = np.array([179, 15, 255])
+        # cv2.imshow("gray", gray)
+
+        hsv = cv2.cvtColor(fblur, cv2.COLOR_BGR2HSV)
+
+        white_lower = np.array([0, 0, 169])
+        white_upper = np.array([255, 255, 255])
 
         mask = cv2.inRange(hsv, white_lower, white_upper)
 
-        cv2.imshow("players", mask)
+        kernel_player = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+        morph_player = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel_player)
+
+        cv2.imshow("players", morph_player)
 
         mid = cv2.Canny(gray, 30, 100, L2gradient=True)
 
